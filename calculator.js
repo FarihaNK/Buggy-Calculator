7107798fd6098e4acb65e7cedafc699b47099cd1
Fariha Khan <nawed.khan7904@gmail.com>
2023-10-11 03:17:59 -0400

    adding function parameter

--- calculator.js
+++ calculator.js
@@ -1,31 +1,12 @@
-function oneFunction() {
-    if (document.getElementById("buttonid") == n1){
-    document.getElementById("buttonid").className = "button2";}
-  else {document.getElementById("buttonid").className = "button1";}
+let expression = "";
+function myFunction(value) {
+    if (value != "="){
+        expression += value
+        const typerElement = document.getElementById("typer");
+        typerElement.textContent = expression
+    } else {otherFunction()}
   }
-  
-  let y = "";
-  function myFunction() {
-    x = document.getElementById('typer').textContent;
+
+  function otherFunction() {
     
-    if (document.getElementById() == "n1"){
-        x.concat(x, "1");
-    }else if (document.getElementById() == "n2"){
-        x.concat(x, "2");
-    }else if (document.getElementById() == "n3"){
-        x.concat("3");
-    }else if (document.getElementById() == "n4"){
-        x.concat("4");
-    }else if (document.getElementById() == "n5"){
-        x.concat("5");
-    }else if (document.getElementById() == "n6"){
-        x.concat("6");
-    }else if (document.getElementById() == "n7"){
-        x.concat("7");
-    }else if (document.getElementById() == "n8"){
-        x.concat("8");
-    }else if (document.getElementById() == "n9"){
-        x.concat("9");
-    }else if (document.getElementById() == "n0"){
-        x.concat("0");}
   }
