c990e8e7caaa60ac31877ae0e4e6466add78d3ba
Fariha Khan <nawed.khan7904@gmail.com>
2023-10-10 21:58:59 -0400

    changes to js file

---
+++ calculator.js
@@ -0,0 +1,31 @@
+function oneFunction() {
+    if (document.getElementById("buttonid") == n1){
+    document.getElementById("buttonid").className = "button2";}
+  else {document.getElementById("buttonid").className = "button1";}
+  }
+  
+  let y = "";
+  function myFunction() {
+    x = document.getElementById('typer').textContent;
+    
+    if (document.getElementById() == "n1"){
+        x.concat(x, "1");
+    }else if (document.getElementById() == "n2"){
+        x.concat(x, "2");
+    }else if (document.getElementById() == "n3"){
+        x.concat("3");
+    }else if (document.getElementById() == "n4"){
+        x.concat("4");
+    }else if (document.getElementById() == "n5"){
+        x.concat("5");
+    }else if (document.getElementById() == "n6"){
+        x.concat("6");
+    }else if (document.getElementById() == "n7"){
+        x.concat("7");
+    }else if (document.getElementById() == "n8"){
+        x.concat("8");
+    }else if (document.getElementById() == "n9"){
+        x.concat("9");
+    }else if (document.getElementById() == "n0"){
+        x.concat("0");}
+  }
