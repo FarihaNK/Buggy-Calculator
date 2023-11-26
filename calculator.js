e86d2f0a50302461653f67b16ffdaf56490067eb
FariaNK <nawed.khan7904@gmail.com>
2023-11-26 17:39:15 -0500

    fix appearance, read expression

--- calculator.js
+++ calculator.js
@@ -1,12 +1,30 @@
-let expression = "";
+var expression = "";
 function myFunction(value) {
-    if (value != "="){
-        expression += value
-        const typerElement = document.getElementById("typer");
-        typerElement.textContent = expression
-    } else {otherFunction()}
+    expression += value;
+    const typerElement = document.getElementById("typer");
+    typerElement.textContent = expression;
+    if (value == "="){otherFunction();}
   }
 
+  var question = [];
+  var startindex = 0;
   function otherFunction() {
-    
+    for (let i = 0; i < expression.length; i++){
+        const temp = "+-x/=";
+        if (temp.includes(expression[i])) {
+            helperFunction(expression[i], i);
+        }
+        else if (expression[i] == "=") {
+            question.pop();
+        }
+    }
+    console.log(question);
   }
+
+  function helperFunction(string, int) {
+    question.push(Number(expression.substring(startindex, int)));
+    question.push(string);
+    startindex = int+1;
+  }
+
+  function anotherFunction(){}
