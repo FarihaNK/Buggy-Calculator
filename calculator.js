5c2a16220176c7e9b40adef5f598db58911cca0d
FariaNK <nawed.khan7904@gmail.com>
2023-11-26 19:17:49 -0500

    finish calculator

--- calculator.js
+++ calculator.js
@@ -1,30 +1,12 @@
+
 var expression = "";
 function myFunction(value) {
     expression += value;
     const typerElement = document.getElementById("typer");
     typerElement.textContent = expression;
-    if (value == "="){otherFunction();}
-  }
-
-  var question = [];
-  var startindex = 0;
-  function otherFunction() {
-    for (let i = 0; i < expression.length; i++){
-        const temp = "+-x/=";
-        if (temp.includes(expression[i])) {
-            helperFunction(expression[i], i);
-        }
-        else if (expression[i] == "=") {
-            question.pop();
-        }
+    if (value == "="){
+        expression = expression.substring(0, expression.length - 1);
+        let answer = eval(expression);
+        typerElement.textContent = answer;
     }
-    console.log(question);
   }
-
-  function helperFunction(string, int) {
-    question.push(Number(expression.substring(startindex, int)));
-    question.push(string);
-    startindex = int+1;
-  }
-
-  function anotherFunction(){}
